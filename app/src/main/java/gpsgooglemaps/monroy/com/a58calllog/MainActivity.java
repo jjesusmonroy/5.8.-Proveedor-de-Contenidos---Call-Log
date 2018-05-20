1328cb80d45cd464e6ebcc9fcf60807d0b1f9057
jjesusmonroy <ppchuyjjms@gmail.com>
2018-05-20 13:30:19 -0600

    1st commit

---
+++ app/src/main/java/gpsgooglemaps/monroy/com/a58calllog/MainActivity.java
@@ -0,0 +1,13 @@
+package gpsgooglemaps.monroy.com.a58calllog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
